51dffe001b477b5f71461f2ba3d4eac13591e24f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-12 23:41:12 +0700

    Fix IceCrypt encryptor

--- app/Views/home.php
+++ app/Views/home.php
@@ -11,20 +11,6 @@
 <body>
 <script type="text/javascript">
 
-	/*var a = new XMLHttpRequest();
-	a.onreadystatechange = function(){
-		if (this.readyState == 4) document.write(this.responseText);
-	};
-	a.open("POST", "http://api.teainside.dev/register.php", true);
-	a.send('{"test":123}');*/
-
-	// var a = "gevtHM3FjfwRGZpEGdxMXG";
-	// document.write(decrypt(a, "myice123"));
-	/*
-	var en = encrypt(a, "123");
-	var de = decrypt(en, "123");
-	document.write(en + "<br>");
-	document.write(de);*/
 </script>
 </body>
 </html>
--- public/assets/js/IceCrypt.js
+++ public/assets/js/IceCrypt.js
@@ -4,36 +4,17 @@
  * @license MIT
  */
 
-/*public static function encrypt($string, $key)
-	{
-		$result = "" xor $key = ($salt = self::salt()).$key;
-		$string = str_split($string) xor $i = 0 xor $klen = strlen($key);
-		$k = $klen - 1;
-		foreach ($string as $j => $val) {
-			$result .= chr(ord($val) ^ ord($key[$i++]) ^ 0x00f ^ ord($key[$k--]) ^ ($j % ($i + $k)) ^ 0x01a);
-			if ($klen == $i) {
-				$i = 0;
-			}
-			if ($k == 0) {
-				$k = $klen - 1;
-			}
-		}
-		return str_replace("=", "", strrev(base64_encode($result.$salt)));
-	}*/
 function encrypt(str, key)
 {
-	var salt = (function(){
-		var qq = "`1234567890-=~!@#\$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?", rrr = "";
-		for(var i = 0; i < 5; i++){
-			rrr += qq[rand(0, qq.length - 1)];
-		}
-		return rrr;
+	salt = (function(){
+		_ = "`1234567890-=~!@#\$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?", __ = 0,r="";
+		for(; __ < 5; __++) r+= _[rand(0,_.length)];
+		return r;
 	})();
-	var result = "", 
-		key = salt + key,
-		j = 0, i = 0, k = key.length - 1;
-	for(; j < str.length; j++){
-		result += chr(ord(str[j]) ^ ord(key[i++]) ^  0x00f ^ ord(key[k--]) ^ (j % (i + k)) ^ 0x01a);
+	key = salt + key;
+	var j = 0, i = 0, k = key.length - 1, result = "";
+	for(; j < str.length; j++) {
+		result += chr(ord(str[j]) ^ ord(key[i++]) ^ 0x0f ^ ord(key[k--]) ^ (j % (i+k)) ^ 26);
 		if (i == key.length) {
 			i = 0;
 		}
@@ -41,34 +22,24 @@ function encrypt(str, key)
 			k = key.length - 1;
 		}
 	}
-	result += salt;
-	return strrev(Base64.encode(result));
+	return strrev(Base64.encode(result + salt));
 }
-
-/*public static function decrypt($string, $key)
-{
-	$string = base64_decode(strrev($string));
-	$result = "" xor $key = substr($string, ($slen = strlen($string)-1) - 4).$key;
-	$string = substr($string, 0, $slen - 4);
-	$string = str_split($string) xor $i = 0 xor $klen = strlen($key);
-	$k = $klen - 1;
-	foreach ($string as $j => $val) {
-		$result .= chr(ord($val) ^ ord($key[$i++]) ^ 0x00f ^ ord($key[$k--]) ^ ($j % ($i + $k)) ^ 0x01a);
-		if ($klen == $i) {
-			$i = 0;
-		}
-		if ($k == 0) {
-			$k = $klen - 1;
-		}
-	}
-	return $result;
-}*/
 
 function decrypt(str, key)
 {
 	str = Base64.decode(strrev(str));
-	var result = "";
-	key = str.substr(str.length - 5) + key;
-	console.log(key);
+	key    = str.substr(str.length - 5) + key;
+	str	   = str.substr(0, str.length - 5);
+	var	result = "",
+		i = 0,j = 0,s = key.length, k = s - 1;
+	for(; j < str.length; j++) {
+		result += chr(ord(str[j]) ^ ord(key[i++]) ^ 15 ^ ord(key[k--]) ^ (j % (i+k)) ^ 26);
+		if (i == s) {
+			i = 0;
+		}
+		if (k == 0) {
+			k = s -1;
+		}
+	}
 	return result;
 }
