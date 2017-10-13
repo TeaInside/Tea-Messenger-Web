779e1b16a4bf283aeec65f0180ba3889a5c200cc
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-13 17:22:30 +0700

    Prepare register form

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Register Tea Messenger</title>
+	<script src="<?php print js("base64"); ?>" type="text/javascript"></script>
+	<script src="<?php print js("helpers"); ?>" type="text/javascript"></script>
+	<script src="<?php print js("IceCrypt"); ?>" type="text/javascript"></script>
+	<script src="<?php print js("register"); ?>" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="<?php print css("register"); ?>">
 </head>
 <body>
 	<!-- 
@@ -11,14 +16,72 @@
 	   JS  		public/assets/js
 	   Gambar	public/assets/img
 	   Lainnya	public/assets/{buat folder sendiri}
-	2. Buat form pendaftaran.
-		Field yang dibutuhkan 
-		- First name (required)
-		- Last name (optional)
-		- Email (required)
-		- Phone (required)
-		- Gender (required) 
 	-->
-	<h2>Buat register disini</h2>
+	<center>
+	<div id="frcg">
+		<form method="post" action="javascript:void(0);" id="fr">
+			<table>
+				<thead>
+					<tr><th colspan="3" align="center">Register Tea Messenger</th></tr>
+				</thead>
+				<tbody>
+					<tr><td>First name</td><td>:</td><td><input type="text" name="fn" id="first_name"></td></tr>
+					<tr><td>Last name</td><td>:</td><td><input type="text" name="ln" id="last_name"></td></tr>
+					<tr><td>E-Mail</td><td>:</td><td><input type="email" name="e" id="email"></td></tr>
+					<tr><td>Phone</td><td>:</td><td><input type="text" name="p" id="phone"></td></tr>
+					<tr><td>Gender</td><td>:</td><td>
+						<input type="radio" name="g" id="g1" value="male"> Male
+						<input type="radio" name="g" id="g2" value="female"> Female
+					</td></tr>
+				</tbody>
+				<thead>
+					<tr><th colspan="3" align="center">Create Username</th></tr>
+				</thead>
+				<tbody>
+					<tr><td>Username</td><td>:</td><td><input type="text" name="u" id="username"></td></tr>
+					<tr><td>Password</td><td>:</td><td><input type="password" name="p" id="password"></td></tr>
+					<tr><td>Confirm Password</td><td>:</td><td><input type="password" name="cp" id="cpassword"></td></tr>
+				</tbody>
+				<tfoot>
+					<tr><td colspan="3" align="center">
+						<div>
+							<p>Captcha</p>
+							<!-- Nantinya disini kita kasih captcha -->
+						</div>
+					</td></tr>
+					<tr><td colspan="3" align="center">
+						<button id="sbt">Register</button>
+					</td></tr>
+				</tfoot>
+			</table>
+		</form>
+	</div>
+	</center>
+	<script type="text/javascript">
+		document.getElementById('fr').addEventListener("submit", function(){
+			var q = new register();
+			q.getInput();
+			if (q.formValidator()) {
+				q.buildData();
+				q.send("http://api.teainside.dev/register.php", function(res){
+					try {
+						res = JSON.parse(res);
+						if (typeof res['error_message'] == undefined && res['status'] == true) {
+							alert(res['message']);
+							window.location = res['redirect_to'];
+						} else if(typeof res['error_message'] != undefined) {
+							alert(res['error_message']);
+						} else {
+							alert("Unknown error!");
+							window.location = "";
+						}
+					} catch (e) {
+						alert("Error : " + e.message);
+						window.location = "";
+					}
+				});
+			}
+		});
+	</script>
 </body>
 </html>
---
+++ public/assets/js/register.js
@@ -0,0 +1,134 @@
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
+class register
+{
+	constructor()
+	{
+		this.input = null;
+		this.data  = null;
+		this.validator = {
+			"first_name": function(str){
+				return true;
+			},
+			"last_name": function(str){
+				return true;
+			},
+			"email": function(str){
+				// HTML auto validator (udah)
+				return true;
+			},
+			"phone": function(str){
+				if (str.length < 10 || str.length > 20) {
+					alert("Invalid phone number!");
+					return false;
+				}
+				return true;
+			},
+			"gender": function(str){
+				if (str=="male" || str=="female") {
+					return true;
+				}
+				alert("Invalid gender!"); // avoid inspect element 
+				return false;
+			},
+			"username": function(str){
+				// validasi username
+				/**
+				 * 1. Username minimal 4 karakter
+				 * 2. Username maksimal 32 karakter
+				 * 3. Username hanya boleh terdiri dari karakter A-Z a-z 0-9 dan underscore
+				 */
+				return true;
+			},
+			"password": function(str){
+				// validasi password
+				/**
+				 * 1. Password minimal 6 karakter
+				 * 2. Password maksimal tidak dibatasi
+				 * 3. Password hanya boleh terdiri dari printable chars ASCII (32 - 127)
+				 *
+				 * Hint no 3 :
+				 * cek public/assets/js/helpers.js
+				 */
+				// alert("Password too short, please provide password more than 5 digit chars!");
+				// alert("Invalid password, non printable chars are not allowed!");
+				return true;
+			},
+			"cpassword": function(str){
+				// special validator
+				/**
+				 * Karena di javascript pada closurenya mereferensikan var `this` pada closure itu sendiri, 
+				 * maka kita tidak bisa menggunakan instance class register dengan var `this`.
+				 * Karena itu kita diharuskan melakukan instansiasi class register pada self class.
+				 * Hal ini tentunya akan boros memory. Maka khusus input cpassword kita perlakukan spesial.
+				 * Validator input cpassword ada di method register.formValidator
+				 */
+				return true;		
+			}
+		};
+	}
+
+	gv(id){
+		return document.getElementById(id).value;
+	}
+
+	getInput()
+	{
+		this.input = {
+			"first_name": 	this.gv("first_name"), 
+			"last_name": 	this.gv("last_name"), 
+			"email": 		this.gv("email"), 
+			"phone":		this.gv("phone"),
+			"gender":		(function(){
+								if (document.getElementById('g1').checked) {
+  									return document.getElementById('g1').value;
+								} else if(document.getElementById('g2').checked) {
+									return document.getElementById('g2').value;
+								}
+								return null;
+							})(),
+			"username":		this.gv("username"),
+			"password":		this.gv("password"),
+			"cpassword":	this.gv("cpassword")
+		};
+		console.log(this.input['gender']);
+	}
+
+	formValidator()
+	{
+		var g;
+		for(g in this.validator) {
+			if(! this.validator[g](this.input[g])) {
+				return false;
+			}
+		}
+		if (this.input['cpassword'] != this.input['password']) {
+			alert("Password does not match the confirm password!");
+			return false;
+		}
+		return true;
+	}
+
+	buildData()
+	{
+		this.data = JSON.stringify({
+			"key_build": encrypt("icetea", "teainside"),
+			"data": encrypt(JSON.stringify(this.input), "icetea")
+		});
+	}
+
+	send(url, closure)
+	{
+		var a = new XMLHttpRequest();
+		a.open("POST", url, true);
+		a.onreadystatechange = function(){
+			if (this.readyState == 4) {
+				closure(this.responseText);
+			}
+		};
+		a.send(this.data);
+	}
+}
