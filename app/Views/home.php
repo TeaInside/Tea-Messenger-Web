c311fd22b3423acc0bdfbd4fb2d5d7cf3c2562e3
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-15 19:18:07 +0700

    Update home test

--- app/Views/home.php
+++ app/Views/home.php
@@ -10,7 +10,10 @@
 </head>
 <body>
 <script type="text/javascript">
-
+	var a = encrypt('{"first_name":"Ammar","last_name":"Faizi","email":"ammarfaizi2@gmail.com","phone":"085867152777","gender":"male","username":"ammarfaizi2","password":"123123","cpassword":"123123"}', "123123");
+	var b = decrypt(a, "123123");
+	console.log(a);
+	console.log(b);
 </script>
 </body>
 </html>
