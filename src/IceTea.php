d5381a60c52db12f19b2b25fb780a808dae79630
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-15 20:12:03 +0700

    Update framework core

--- src/IceTea.php
+++ src/IceTea.php
@@ -12,9 +12,13 @@ final class IceTea
     public static function run()
     {
         foreach (Router::loadRoutes() as $key => $route) {
-            if (Router::action($key, $route)) {
+            if ($not_found = Router::action($key, $route)) {
                 break;
             }
+        }
+        if (! $not_found) {
+        	http_response_code(404);
+        	return view("errors/404");
         }
     }
 }
