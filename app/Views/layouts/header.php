6bd2fa2df9658e2af587e446fb049b1bfe0e6ef4
egin10 <wweusz@gmail.com>
2017-10-20 14:17:40 +0700

    edit color

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tea Messenger</title>
+    <link rel="shortcut icon" href="<?php print img("logo-ice-tea"); ?>" />
     
     <!-- CSS -->
     <link rel="stylesheet" href="<?php print css("bootstrap"); ?>">
