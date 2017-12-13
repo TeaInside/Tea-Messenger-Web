b419260a184aff64cf0fd0281c2a3bcbc6d222e9
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-13 19:08:49 +0700

    Create composer modification

--- framework/icetea.php
+++ framework/icetea.php
@@ -18,16 +18,16 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
 
     }
 
-
+    // Load helpers
+    $scan = scandir(__DIR__.'/helpers');
+    unset($scan[0], $scan[1]);
+    foreach ($scan as $file) {
+        include __DIR__.'/helpers/'.$file;
+    }
     spl_autoload_register('___loadClass');
 }
 
-// Load helpers
-$scan = scandir(__DIR__.'/helpers');
-unset($scan[0], $scan[1]);
-foreach ($scan as $file) {
-    include __DIR__.'/helpers/'.$file;
-}
+
 
 $app = new IceTea\IceTea();
 $app->build();
---
+++ framework/init/composer.php
@@ -0,0 +1,7 @@
+<?php
+// Load helpers
+$scan = scandir(__DIR__.'/helpers');
+unset($scan[0], $scan[1]);
+foreach ($scan as $file) {
+    include __DIR__.'/../helpers/'.$file;
+}
