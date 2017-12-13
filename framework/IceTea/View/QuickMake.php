444139049718976b20375646de2342fd406396e7
Louvian Lyndal <louvianlyndal@gmail.com>
2017-12-13 13:08:58 +0700

    Fix quickmake

--- framework/IceTea/View/QuickMake.php
+++ framework/IceTea/View/QuickMake.php
@@ -2,10 +2,36 @@
 
 namespace IceTea\View;
 
+use IceTea\Hub\Singleton;
+use IceTea\Support\View\PosibleFile;
+use IceTea\Exceptions\ViewException;
+
 class QuickMake
 {
-	public static function make()
+	use Singleton, PosibleFile;
+
+	public static function make($name, $type)
 	{
-		
+		switch ($type) {
+			case 'layout':
+				return self::probablility('layouts/'.$name);
+				break;
+			
+			default:
+				break;
+		}
+	}
+
+	private static function probablility($name)
+	{
+		$ins = self::getInstance();
+		if ($file = $ins->teaFile($name)) {
+			return $file;
+		} elseif ($file = $ins->bladeFile($name)) {
+			return $file;
+		} elseif ($file = $ins->phpNativeFile($name)) {
+			return $file;
+		}
+		throw new ViewException("View [$name] not found");
 	}
 }
