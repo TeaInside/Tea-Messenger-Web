c92fb68dc9b4e3dde25dd58634a4ca4af3ce2473
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-13 16:14:45 +0700

    Fix abort helper

--- public/assets/js/auth/register.js
+++ public/assets/js/auth/register.js
@@ -8,7 +8,10 @@ class register
 	{
 		var that = this;
 		domId('form-register').addEventListener("submit", function () {
-			that.action(123);
+			var context = that.buildContext();
+			if (context !== false) {
+				that.action(context);
+			}
 		});
 	}
 	action(dt){
@@ -21,5 +24,20 @@ class register
 			ch.open("POST", this.to);
 			ch.send(dt);
 			alert(123);
+	}
+	buildContext()
+	{
+		var q = {
+			"first_name": domId('fn').value,
+			"last_name": domId('ln').value,
+			"email": domId('email').value,
+			"username": domId('uname').value,
+			"password": domId('pass').value,
+			"cpassword": domId('cpass').value
+		};
+		if (q['password']!==q['cpassword']) {
+			alert("Confirm password does not match!");
+			return false;
+		}		
 	}
 }
