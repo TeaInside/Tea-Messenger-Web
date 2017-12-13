95146b85cf73cf3ac43545514cf01f242add56db
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-13 19:10:19 +0700

    Fix composer modification

--- framework/init/composer.php
+++ framework/init/composer.php
@@ -1,6 +1,6 @@
 <?php
 // Load helpers
-$scan = scandir(__DIR__.'/helpers');
+$scan = scandir(__DIR__.'/../helpers');
 unset($scan[0], $scan[1]);
 foreach ($scan as $file) {
     include __DIR__.'/../helpers/'.$file;
