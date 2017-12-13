d57ab2b351de1575dd995a4ffb3da5e90b9a1c7d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-13 21:07:48 +0700

    Update home

--- app/Views/user/home.tea.php
+++ app/Views/user/home.tea.php
@@ -2,8 +2,15 @@
 <html>
 <head>
 	<title>Welcome, {{ $info['first_name'] }}!</title>
+	<style type="text/css">
+		h1 {
+			font-family: Helvetica;
+		}
+	</style>
 </head>
 <body>
-
+<center>
+	<h1>Welcome to Tea Messenger</h1>
+</center>
 </body>
 </html>
