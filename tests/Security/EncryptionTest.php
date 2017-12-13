a202edea361239158d70ed4a28d4a11b1e0fb52e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-13 19:22:51 +0700

    Update encryption test

--- tests/Security/EncryptionTest.php
+++ tests/Security/EncryptionTest.php
@@ -24,22 +24,24 @@ class EncryptionTest extends TestCase
 
 	public function testRandomString()
 	{
-		$r = "";
-		for ($i=1; $i < 255; $i++) { 
-			$r .= chr($i);
-		}
-		// generate random string
-		$string = rstr(32, $r);
+		for ($l=0; $l < 5; $l++) { 
+			$r = "";
+			for ($i=1; $i < 255; $i++) { 
+				$r .= chr($i);
+			}
+			// generate random string
+			$string = rstr(32, $r);
 
-		for ($i=0; $i < 200; $i++) { 
-			// generate random key
-			$key = rstr(32, $r);
+			for ($i=0; $i < 200; $i++) { 
+				// generate random key
+				$key = rstr(32, $r);
 
-			$encrypted = IceCrypt::encrypt($string, $key);
-			$this->assertEquals(
-				IceCrypt::decrypt($encrypted, $key),
-				$string
-			);
+				$encrypted = IceCrypt::encrypt($string, $key);
+				$this->assertEquals(
+					IceCrypt::decrypt($encrypted, $key),
+					$string
+				);
+			}
 		}
 	}
 }
