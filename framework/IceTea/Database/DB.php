3fc596dc3dae20d83585d8ea595ea2da83618cfc
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-14 15:35:53 +0700

    PSR2 coding standard fixer

--- framework/IceTea/Database/DB.php
+++ framework/IceTea/Database/DB.php
@@ -8,33 +8,33 @@ use IceTea\Hub\Singleton;
 
 class DB
 {
-	use Singleton;
+    use Singleton;
 
-	/**
-	 * @var \PDO
-	 */
-	protected $pdo;
+    /**
+     * @var \PDO
+     */
+    protected $pdo;
 
-	/**
-	 * Constructor.
-	 *
-	 */
-	public function __construct()
-	{
-		$config = Config::get('database');
-		$this->pdo = new PDO(
-			$config['driver'].":host=".$config['host'].";dbname=".$config['dbname'].";port=".$config['port'],
-			$config['user'],
-			$config['pass']
-		);
-	}
+    /**
+     * Constructor.
+     *
+     */
+    public function __construct()
+    {
+        $config = Config::get('database');
+        $this->pdo = new PDO(
+            $config['driver'].":host=".$config['host'].";dbname=".$config['dbname'].";port=".$config['port'],
+            $config['user'],
+            $config['pass']
+        );
+    }
 
-	/**
-	 * @param string $method
-	 * @param array  $parameters
-	 */
-	public static function __callStatic($method, $parameters)
-	{
-		return self::getInstance()->pdo->{$method}(...$parameters);
-	}
+    /**
+     * @param string $method
+     * @param array  $parameters
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        return self::getInstance()->pdo->{$method}(...$parameters);
+    }
 }
