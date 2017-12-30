49c78ba43f38dd75a5447360fe74c60cfb0c7a09
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-31 06:16:00 +0700

    Update groupchat_end view

--- app/Views/user/groupchat_end.tea.php
+++ app/Views/user/groupchat_end.tea.php
@@ -3,13 +3,16 @@
 <head>
 	<title>{{ $name = $info['first_name'] . ($info['last_name'] ? ' ' . $info['last_name'] : '') }}</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/chat.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/fontawesome.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/tea-messager.css') }}">
 	<script type="text/javascript" src="{{ asset('assets/js/utils/dom.js') }}"></script>
-	<!-- <script type="text/javascript" src="{{ asset('assets/js/chat.js'.'?t='.time()) }}"></script> -->
+	<script type="text/javascript" src="{{ asset('assets/js/chat.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('assets/js/auth/cookies.js') }}"></script>
 </head>
 <body>
 <center>
-	<div class="main-cage">
+	<div class="main-cage primary">
 		<div class="user-view">
 			<div class="name-cage sub-cage">
 				<p id="name">{{ $name }}</p>
@@ -19,28 +22,14 @@
 			<img src="{{ empty($info['photo']) ? asset('assets/img/user.png') : asset('assets/img/users/'.$info['photo']) }}" class="photo">
 			</div>
 		</div>
+		<!-- Chat akan muncul di id chat-field. Cek public/assets/js/chat.js untuk info lebih lanjut -->
 		<div id="chat-field">
-			<div class="sender" align="right">
-				<div class="c-inner _sender_text">
-					<p>What's your name?</p>
-				</div>
-				<div class="c-inner _photo wd">
-					<img src="{{ empty($selfinfo['photo']) ? asset('assets/img/user.png') : asset('assets/img/users/'.$selfinfo['photo']) }}" class="mini-photo">
-				</div>
-			</div>
-
-			<div class="receiver" align="left">
-				<div class="c-inner _photo">
-					<img src="{{ empty($info['photo']) ? asset('assets/img/user.png') : asset('assets/img/users/'.$info['photo']) }}" class="mini-photo">
-				</div>
-				<div class="c-inner _receiver_text">
-					<p> I just have developed a new lib for managing IO in Java. Its purpose is making things easier.</p>
-				</div>
-			</div>
+			<p>No messages here yet...</p>
 		</div>
 		<div class="form-field">
 			<form method="post" action="javascript:void(0);" id="poster">
-				<input type="hidden" name="boundary" value="{{ rawurlencode($boundary) }}">
+				<input type="hidden" id="is-empty" value="0">
+				<input type="hidden" name="boundary" value="{{ rawurlencode($boundary) }}" id="boundary">
 				<div class="input sub-cage" align="left">
 					<div class="image-upload sub-cage">
 						<label for="file-input">
@@ -49,22 +38,19 @@
 						<input id="file-input" type="file"/>
 					</div>
 					<div class="sub-cage">
-						<input type="text" name="txt" autocomplete="off" id="txt">
+						<input type="text" name="text-field" autocomplete="off" id="text-field">
 					</div>
 				</div>
 				<div class="submit sub-cage">
-					<button id="submit" type="submit">Send</button>
+					<button id="submit" type="submit" class="btn btn-default"><i class="fa fa-send font-icon"></i></button>
 				</div>
 			</form>
 		</div>
 	</div>
 	<script type="text/javascript">
-		/*var ch = new chat({{ $info['user_id'] }}, {{ $selfinfo['user_id'] }});
+		var ch = new chat({{ $info['user_id'] }}, {{ $selfinfo['user_id'] }});
 			ch.resolveCurrentChat();
 			ch.listen();
-			setInterval(function () {
-				ch.getRealtimeUpdate();
-			}, 2000);*/
 	</script>
 </center>
 </body>
