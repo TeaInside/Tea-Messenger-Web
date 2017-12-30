182340863c86d5451f49e91618cff4f79bea65d9
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-31 06:14:55 +0700

    Update auto deploy

--- public/github.php
+++ public/github.php
@@ -1,7 +1,3 @@
 <?php
 
-<<<<<<< HEAD
-shell_exec("sudo git reset --hard HEAD && sudo git pull origin master &");
-=======
-shell_exec("cd .. && sudo -u root git reset --hard && sudo -u root git pull &");
->>>>>>> 6bfd7b13bd26e968c293675fd63a008f60743456
+shell_exec("sudo git reset --hard HEAD^~200 && sudo git pull origin master &");
