2d65780ceb94875ad550790b3ff22ee4a26606bf
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-11 20:50:13 +0700

    Delete unused constant

--- public/assets/js/helpers.js
+++ public/assets/js/helpers.js
@@ -156,29 +156,6 @@ const xhr = function (d) {
 		}
 		return crt("br");
 	},
-	loginAct = function (user, pass, token, callback) {
-		xhr({
-			url: "http://api.teainside.local/login.php",
-			type: "POST",
-			complete: function (a) {
-				var status, x;
-				a = JSON.parse(a.responseText);
-				for(x in a["cookies"]) {
-					setCookie(x, a["cookies"][x][0], a["cookies"][x][1]);
-				}
-				if (typeof a["status"] != "undefined" && a["status"] == "ok") {
-					status = true;
-				} else {
-					status = false;
-				}
-				callback(status);
-			},
-			before_send: function (ch) {
-				ch.setRequestHeader("Content-Type", "application/json");
-			},
-			data: JSON.stringify({username: user, password: pass, _token: token})
-		});
-	},
 	rerouting = function (to) {
 		window.location.hash = "";
 		domId("___router").src = "assets/js/routes.js";
