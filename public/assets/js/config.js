ff29e2a51729f7e582ad5e5058ea5c77d97f6b09
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-13 16:15:04 +0700

    Add config.js

---
+++ public/assets/js/config.js
@@ -0,0 +1,4 @@
+const config = {
+	api_url: "http://api.teainside.local",
+	root_domain: "http://messenger.teainside.local"
+};
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,9 +7,11 @@ var routes = doc().createElement("script");
 		route_handle(); 
 	};
 domId("head").appendChild(routes);
-hl("css", "/assets/css/bootstrap.min.css");
-hl("js", "/assets/js/third_party/jquery.min.js", function () {
-	hl("js", "/assets/js/third_party/bootstrap.min.js");
+hl("js", "/assets/js/config.js", function () {
+	hl("css", "/assets/css/bootstrap.min.css");
+	hl("js", "/assets/js/third_party/jquery.min.js", function () {
+		hl("js", "/assets/js/third_party/bootstrap.min.js");
+	});	
 });
 window.addEventListener("hashchange", function() {
 	domId("head").innerHTML = " <meta charset=\"UTF-8\"><title></title>";
