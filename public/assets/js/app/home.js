be0e9a667f221dbf5addd7e29c9ce8e463315d05
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-31 17:37:56 +0700

    [a01] Prepare home styling and update creator.js

--- public/assets/js/app/home.js
+++ public/assets/js/app/home.js
@@ -10,15 +10,39 @@ class home extends Component
 	constructor(props) {
 		super(props);
 		setTitle("Tea Messenger");
+		loadCss(asset("css/home.css"), 0, 1);
 	}
 	render() {
 		var 
 			rax = crt("center"),
 			rdi = crt("div"), 
-			rsi = crt("h2"), rdx, r8, r9, r10, r11, r12, r13, r14, r15;
+			rsi = crt("h2"),
+			rdx = crt("div"),
+			r8 = crt("img"),
+			r9 = crt("div"),
+			r10 = crt("table"), r11, r12, r13, r14, r15,
+			rbp = {
+				"First Name": "first_name",
+				"Last Name": "last_name",
+				"Email": "email",
+				"Phone": "phone"
+			};
 
+		for(r11 in rbp) {
+			r12 = crt("tr");
+			r13 = crt("td");
+			r13.ac(crn(r11));
+			r12.ac(r13, crt("td").ac(crn(":")));
+			r10.ac(r12);
+		}
+
+		r9.ac(r10);
+		r8.src = "";
+		r8.id = "uimg";
+		rdx.ac(r8);
 		rsi.id = "hll";
-		rdi.ac(rsi);
+		rdi.ac(rsi, rdx, r9);
+		rdi.id = "caged";
 		rax.ac(rdi);
 
 		return (
@@ -38,10 +62,11 @@ const get_user_info = function () {
 		url: config.api_url+"/home.php?action=get_user_info",
 		complete: function (r) {
 			try {
-				al(r.responseText);
 				r = JSON.parse(r.responseText);
-				domId();
-				if (r["status"] !== "success") {
+				if (r["status"] === "success") {
+					r = r["data"];
+					domId("hll").innerHTML = "Hello "+r["first_name"]+" "+r["last_name"]+"!";
+				} else {
 					localStorage.removeItem("token_session");
 					reroute("login");
 				}
