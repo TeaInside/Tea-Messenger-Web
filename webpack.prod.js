7454d8535ca677648a6b55823c66146aa662aec1
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-03-16 23:22:50 +0700

    Moving CopyWebpackPlugin and CleanWebpackPlugin config from common config to production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
@@ -12,7 +14,18 @@ module.exports = merge(baseConfig, {
       'process.env': {
         NODE_ENV: JSON.stringify('production')
       },
-    })
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin(
+      [
+        { from: './src/assets', to: 'assets' },
+        { from: './node_modules/@fortawesome/fontawesome-free/css', to: 'assets/vendors/fontawesome/css'},
+        { from: './node_modules/@fortawesome/fontawesome-free/webfonts', to: 'assets/vendors/fontawesome/webfonts'},
+        { from: './node_modules/animate.css/animate.min.css', to: 'assets/vendors/animate.css/'},
+        { from: './node_modules/noty/lib', to: 'assets/vendors/noty'}
+      ],
+      { debug: false}
+    )
   ],
   optimization: {
     minimize: true,
