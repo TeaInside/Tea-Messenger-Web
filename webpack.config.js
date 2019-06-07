fed7bd613cdcc2617db46b88f8c505ee3b49bbae
Ammar Faizi <ammarfaizi2@gmail.com>
2019-06-07 10:19:06 +0700

    Revert "Update configurations"
    This reverts commit d978fd1bcb370fa218f4f2689ead1545b0730cda.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
@@ -24,7 +22,8 @@ module.exports = {
     contentBase: path.join(__dirname, 'src/assets'),
     publicPath: '/',
     historyApiFallback: true,
-    watchContentBase: true
+    watchContentBase: true,
+    compress: true
   },
   plugins: [
     new webpack.ProvidePlugin({
@@ -37,31 +36,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "assets/css/styles.[contenthash].css",
       chunkFilename: "assets/css/[name].[contenthash].css"
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin(
-    [{ 
-      from: './src/assets', 
-      to: 'assets' 
-    },
-    { 
-      from: './node_modules/@fortawesome/fontawesome-free/css', 
-      to: 'assets/vendors/fontawesome/css'
-    },
-    { 
-      from: './node_modules/@fortawesome/fontawesome-free/webfonts', 
-      to: 'assets/vendors/fontawesome/webfonts'
-    },
-    { 
-      from: './node_modules/animate.css/animate.min.css', 
-      to: 'assets/vendors/animate.css/animate.min.css'
-    },
-    { 
-      from: './node_modules/noty/lib', 
-      to: 'assets/vendors/noty'
-    }], 
-    { 
-      debug: false 
     })
   ],
   module: {
@@ -86,10 +60,7 @@ module.exports = {
             }
           },
           {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass')
-            }
+            loader: 'sass-loader'
           }
         ]
       },
@@ -121,5 +92,19 @@ module.exports = {
         }
       }
     ]
+  },
+  optimization: {
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          minChunks: 2
+        }
+      }
+    }
   }
 };
